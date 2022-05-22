0bcce7f07e85ef4bae4846c3ad54e9257acfb58e
NateCarty <natecartylife@gmail.com>
2022-05-22 14:42:20 -0400

    Added comments to script.js

--- ProgressSteps/script.js
+++ ProgressSteps/script.js
@@ -3,11 +3,17 @@ const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
 
+// var for index of currently active step
 let currentActive = 1
 
+
+// when clicking next 
 next.addEventListener('click', () => {
+
+    // change the active step to 1 above current
     currentActive++
 
+    // if we go past the amount of circles, just dont
     if (currentActive > circles.length){
         currentActive = circles.length
     }
@@ -15,8 +21,14 @@ next.addEventListener('click', () => {
     update()
 })
 
+
+// when clicking previous
 prev.addEventListener('click', () => {
+
+    // change the active step to 1 below current
     currentActive--
+
+    // if we go past the first circle, just dont
     if (currentActive < 1){
         currentActive = 1
     }
@@ -24,8 +36,13 @@ prev.addEventListener('click', () => {
     update()
 })
 
+// on click of either button
 function update()   {
+
+    // for each circle we have,
     circles.forEach((circle, index) => {
+
+        // add or remove their active status depending on what index we have
         if (index < currentActive){
             circle.classList.add('active')
         } else {
@@ -33,15 +50,24 @@ function update()   {
         }
     })
 
+    // get all active circles
     const actives = document.querySelectorAll('.active')
 
+    // progress width is a ratio of the amount of circles and amount of active circles
+    // will give us a number 0 - 1, multiply by 100 to get a percent
+    // We use 98 here because 100% extends beyond the last circle
     progress.style.width = (actives.length - 1) / (circles.length - 1) * 98 + '%'
 
+    // if we are on the first circle, disable the previous button
     if (currentActive === 1){
         prev.disabled = true
+
+    // else if we are on the last one, disable the next button    
     } else if (currentActive === circles.length) {
         next.disabled = true
     } else {
+    
+    // else we can go either direction so don't disable either button
         prev.disabled = false
         next.disabled = false
     }
