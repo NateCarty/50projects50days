ae051bba0f36a7cc21d6867e4e245e851ffd8fa4
NateCarty <natecartylife@gmail.com>
2022-05-28 13:07:52 -0400

    Added sound and js to play sounds

--- AudioBoard/script.js
+++ AudioBoard/script.js
@@ -1,9 +1,13 @@
-const sounds = ['fire', 'forest', 'rainstorm']
+const sounds = ['fire', 'forest', 'rainstorm', 'applause']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
 
     document.getElementById('buttons').
     appendChild(btn)
