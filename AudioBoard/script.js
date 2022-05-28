b201ba6cf32f546421b769c86045c82a5df805c0
NateCarty <natecartylife@gmail.com>
2022-05-28 12:58:52 -0400

    Added js to dynamically create buttons

---
+++ AudioBoard/script.js
@@ -0,0 +1,10 @@
+const sounds = ['fire', 'forest', 'rainstorm']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+
+    document.getElementById('buttons').
+    appendChild(btn)
+})
