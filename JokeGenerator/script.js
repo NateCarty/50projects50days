defc469a6c6d2e3fbcc169c815a0d216b22b1b71
NateCarty <natecartylife@gmail.com>
2022-05-29 11:31:07 -0400

    Changed .then to async await

--- JokeGenerator/script.js
+++ JokeGenerator/script.js
@@ -5,15 +5,27 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+async function generateJoke() {
     const headerConfig = {
         headers: {
             'Accept': 'application/json'
         }
     }
-    fetch('https://icanhazdadjoke.com', headerConfig)
-    .then(response => response.json())
-    .then(data => {
-        jokeElement.innerHTML = data.joke
-    })
+    
+    const response = await fetch('https://icanhazdadjoke.com', headerConfig)
+    const data = await response.json()
+    jokeElement.innerHTML = data.joke
 }
+
+// function generateJoke() {
+//     const headerConfig = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', headerConfig)
+//     .then(response => response.json())
+//     .then(data => {
+//         jokeElement.innerHTML = data.joke
+//     })
+// }
