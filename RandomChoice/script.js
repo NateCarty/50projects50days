f007836b9c92ac8c1f9e6475e65b437e61c46e17
NateCarty <natecartylife@gmail.com>
2022-06-01 22:02:33 -0400

    Added js to randomly choose from options

--- RandomChoice/script.js
+++ RandomChoice/script.js
@@ -6,6 +6,14 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (event) => {
     createTags(event.target.value)
+
+    if (event.key === 'Enter') {
+        setTimeout(() => {
+            event.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
 
 function createTags(text){
@@ -19,4 +27,41 @@ function createTags(text){
         tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     });
+}
+
+function randomSelect(){
+    const times = 30
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            deHighlightTag(randomTag)
+        }, 100)
+    }, 100)
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+    }, times * 100)
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag){
+    tag.classList.add('highlight')
+}
+
+function deHighlightTag(tag){
+    tag.classList.remove('highlight')
 }
