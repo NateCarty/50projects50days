a7b4ca7d0c67c0eb9e788dec3bb238192c14f18a
NateCarty <natecartylife@gmail.com>
2022-06-16 22:20:19 -0400

    Added js for big cup

--- DrinkWater/script.js
+++ DrinkWater/script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById("liters")
 const percent = document.getElementById("percentage")
 const remainder = document.getElementById("remainder")
 
+updateBigBoy()
+
 smalls.forEach((cup, index) => {
     cup.addEventListener('click', () => highlightCups(index))
 })
@@ -21,4 +23,29 @@ function highlightCups(index) {
             cup.classList.remove('full')
         }
     })
+
+    updateBigBoy()
+}
+
+function updateBigBoy(){
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smalls.length
+
+    if (fullCups === 0) {
+        percent.style.visibility = 'hidden'
+        percent.style.height = 0
+    } else {
+        percent.style.visibility = 'visible'
+        percent.style.height = `${fullCups / totalCups * 230}px`
+        percent.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if (fullCups === totalCups) {
+        remainder.style.visibility = 'hidden'
+        remainder.style.height = 0
+    } else {
+        remainder.style.visibility = 'visible'
+        liters.innerText = `${2 -(250 * fullCups / 1000)}L`
+
+    }
 }
