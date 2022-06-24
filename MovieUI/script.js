3e03df1a87018600c84953f78414f1dcad190189
NateCarty <natecartylife@gmail.com>
2022-06-23 22:25:26 -0400

    Added image path and search url

--- MovieUI/script.js
+++ MovieUI/script.js
@@ -1,1 +1,3 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=42588584e30b5899427cb2e0217aca72&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=42588584e30b5899427cb2e0217aca72&query="'
