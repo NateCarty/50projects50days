3663ab5802d4dca79ac76d9384ad316c668afdbf
NateCarty <natecartylife@gmail.com>
2022-06-23 22:33:28 -0400

    Added search functionality through API call

--- MovieUI/script.js
+++ MovieUI/script.js
@@ -2,6 +2,9 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=42588584e30b5899427cb2e0217aca72&query="'
 
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
 // Get initial movies
 getMovies(API_URL)
 
@@ -9,4 +12,20 @@ async function getMovies(url) {
     const response = await fetch(url)
     const data = await response.json()
 
+    console.log(data)
 }
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_URL + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
