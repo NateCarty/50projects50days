f0e009098ecda93549fb5bcc92c578aaf2447ea1
Nate Carty <natecartylife@gmail.com>
2022-08-06 15:13:15 -0400

    Added event.location functionality

--- KeyCodeFinder/script.js
+++ KeyCodeFinder/script.js
@@ -1,5 +1,26 @@
 const insert = document.getElementById('insert')
 
+function locationFinder(loc){
+   if (loc == 0){
+        return `Standard (${loc})`;
+   } 
+   if (loc == 1){
+        return `Left (${loc})`;
+    }
+    if (loc == 2){
+        eturn `Right (${loc})`;
+    }
+    if (loc == 3){
+        return `Numpad (${loc})`;
+    }
+    if (loc == 4){
+        return `Mobile (${loc})`;
+    }
+    if (loc == 5){
+        return `Joystick (${loc})`;
+    }
+}
+
 window.addEventListener('keydown', (event) => {
     insert.innerHTML = `
     <div class="big">
@@ -16,5 +37,9 @@ window.addEventListener('keydown', (event) => {
 <div class="key">
     ${event.code}
     <small>event.code</small>
+</div>
+<div class="key">
+    ${locationFinder(event.location)}
+    <small>event.location</small>
 </div>`
 })
