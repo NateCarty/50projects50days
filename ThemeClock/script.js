5427a28d687d20d5ee8ba752c1ddfc9f2eb9fd14
Nate Carty <natecartylife@gmail.com>
2023-07-22 14:53:51 -0400

    Moving clock

--- ThemeClock/script.js
+++ ThemeClock/script.js
@@ -18,3 +18,25 @@ toggle.addEventListener('click', (e) => {
         e.target.innerHTML = 'Light mode'       
     }
 })
+
+function setTime() {
+    const time = new Date()
+    const month = time.getMonth()
+    const day = time.getDay()
+    const hours = time.getHours()
+    const clockHours = hours % 12
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+
+    hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(clockHours, 0, 11, 0, 360)}deg)`
+    minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime, 1000)
